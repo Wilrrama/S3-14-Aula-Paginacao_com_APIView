5986f3da361d41e75102e4af1191ab90631d1f27
wilrrama <wilrrama@hotmail.com>
2023-10-15 21:29:23 -0300

    aula finalizada

--- musicians/views.py
+++ musicians/views.py
@@ -2,20 +2,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from musicians.models import Musician
 from musicians.serializers import MusicianSerializer
+from rest_framework.pagination import PageNumberPagination
 
 
-class MusicianView(APIView):        
+class MusicianView(APIView, PageNumberPagination):
     def post(self, request):
         serializer = MusicianSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        
+
         musician = Musician.objects.create(**serializer.validated_data)
         serializer = MusicianSerializer(musician)
-        
+
         return Response(serializer.data, 201)
-    
+
     def get(self, request):
         musicians = Musician.objects.all()
-        serializer = MusicianSerializer(musicians, many=True)
-        
-        return Response(serializer.data)
+        result_page = self.paginate_queryset(musicians, request, view=self)
+        serializer = MusicianSerializer(result_page, many=True)
+
+        return self.get_paginated_response(serializer.data)
--- project_base/settings.py
+++ project_base/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+# project_base/settings.py
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
+}
